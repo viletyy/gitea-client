b27599f2a573a702db1f8ee2f0dbf490b2f03938
viletyy <yystopf@163.com>
2023-01-18 18:31:48 +0800

    fix: change activitypub module name

--- lib/gitea/api/activitypub.rb
+++ lib/gitea/api/activitypub.rb
@@ -1,6 +1,6 @@
 module Gitea
   module Api
-    module Admin 
+    module Activitypub 
 
       def get_activitypub_user_by_username(username, opt = {})
         @http.get("/activitypub/user/#{username}", opt)
@@ -9,7 +9,7 @@ module Gitea
       def post_activitypub_user_inbox_by_username(username, opt = {})
         @http.post("/activitypub/user/#{username}/inbox", opt)
       end
-      
+
     end # Admin
   end # Api 
 end # Gitea
