3b2a46b64164ab2c8591caca0d708599d461f399
viletyy <yystopf@163.com>
2023-01-18 22:37:24 +0800

    add: hat api function

--- lib/gitea/api/hat/admin.rb
+++ lib/gitea/api/hat/admin.rb
@@ -6,7 +6,6 @@ module Gitea
         def patch_admin_users_by_username(username, opt = {})
           @http.patch("/admin/users/#{username}", opt)
         end
-
       end # Admin
     end # Hat 
   end # Api
